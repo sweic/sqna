bf1fa85c77d130f9901ac0578a05242e9f12e8a8
sweic <101677551+sweic@users.noreply.github.com>
2022-12-22 17:29:12 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -54,7 +54,7 @@ function getEndingLink() {
   }
 
   const client = createWSClient({
-    url: "wss://sqna-production.up.railway.app/"
+    url: "wss://sqna-production.up.railway.app:3001"
   });
 
   return wsLink<AppRouter>({
